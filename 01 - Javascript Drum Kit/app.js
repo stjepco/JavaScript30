fe0d22ed484162249d6bb6d82ac5f46c29639deb
Stjepan Grgic <grgic.stjepan@gmail.com>
2018-06-15 23:45:23 +0200

    Everything works now

--- 01 - Javascript Drum Kit/app.js
+++ 01 - Javascript Drum Kit/app.js
@@ -3,19 +3,31 @@
 function playSound(dataKey) {
     const audio = document.querySelector(`audio[data-key="${dataKey}"]`);
     if (!audio) return; // Stop the function from running
-    audio.currentTime = 0; // Revinde tostart
+    audio.currentTime = 0; // Rewind to start
     audio.play();
+}
+
+function animateKey(key) {
+    if (!key) return; // Stop function if an unwanted key is pressed/clicked
+    key.classList.add("pop")
+}
+function removeTransition(event) {
+    if (event.propertyName !== "transform") return; // skip if it is not transform TransitionEvent
+    this.classList.remove("pop")
 }
 
 // Play sonds on key press
 window.addEventListener("keydown", function(key) {
-    const audio = document.querySelector(`audio[data-key="${key.keyCode}"]`);
+    const keyButton = document.querySelector(`.key[data-key="${key.keyCode}"]`);
     playSound(key.keyCode);
+    animateKey(keyButton);
 })
 
 // Play sound when clicking
 const drumKeys = document.querySelectorAll(".key")
 drumKeys.forEach(key => key.addEventListener("click", function(event) {
+    animateKey(key);
     playSound(key.dataset.key);
 }))
 
+drumKeys.forEach(key => key.addEventListener("transitionend", removeTransition))
