b35b3f9e6bbda36bcdd7e528b3a26d0dbf4e2a40
Stjepan Grgic <grgic.stjepan@gmail.com>
2018-06-16 18:03:10 +0200

    Watch is workong

--- 02-JSClock/app.js
+++ 02-JSClock/app.js
@@ -8,14 +8,25 @@
 // }, 1000)
 const handSeconds = document.querySelector(".hand.seconds");
 const handMinutes = document.querySelector(".hand.minutes");
+const handHours = document.querySelector(".hand.hours");
 // console.log(handSeconds)
+
+
+let seconds = new Date().getSeconds()
 
 function setDate(){
   const currentTime = new Date();
-  const seconds = currentTime.getSeconds()
-  handSeconds.style.transform = `rotate(${((seconds/60)*360)-90}deg)`;
-  // handSeconds
-  console.log(seconds);
+  
+  handSeconds.style.transform = `rotate(${((seconds/60)*360)+90}deg)`;
+  seconds++;
+
+  // Minutes
+  const minutes = currentTime.getMinutes()
+  handMinutes.style.transform = `rotate(${((minutes/60)*360)-90}deg)`;
+
+  const hours = currentTime.getHours();
+  handHours.style.transform = `rotate(${((hours/12)*360)-90}deg)`;
+  
 }
 
 setInterval(setDate, 1000)
