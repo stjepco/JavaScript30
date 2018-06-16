8590c826ce4e7b7c3ace1ead1a99e6057da15143
Stjepan Grgic <grgic.stjepan@gmail.com>
2018-06-16 19:52:16 +0200

    Logic refactor to set time on init

--- 02-JSClock/app.js
+++ 02-JSClock/app.js
@@ -1,32 +1,33 @@
 // app.js
 
-// console.log(today);
-// let today = new Date()
-// setInterval(function() {
-
-//   console.log(today.getTime);
-// }, 1000)
+// Get handles
 const handSeconds = document.querySelector(".hand.seconds");
 const handMinutes = document.querySelector(".hand.minutes");
 const handHours = document.querySelector(".hand.hours");
-// console.log(handSeconds)
 
+// Get curent Date and time
+const currentDay = new Date();
+let seconds = currentDay.getSeconds();
+let minutes = currentDay.getMinutes();
+let hours = currentDay.getHours();
 
-let seconds = new Date().getSeconds()
-
-function setDate(){
-  const currentTime = new Date();
-  
-  handSeconds.style.transform = `rotate(${((seconds/60)*360)+90}deg)`;
-  seconds++;
-
-  // Minutes
-  const minutes = currentTime.getMinutes()
-  handMinutes.style.transform = `rotate(${((minutes/60)*360)-90}deg)`;
-
-  const hours = currentTime.getHours();
-  handHours.style.transform = `rotate(${((hours/12)*360)-90}deg)`;
-  
+function rotateHandle(handle, deg) {
+  handle.style.transform = `rotate(${deg}deg)`;
 }
 
-setInterval(setDate, 1000)
+// Set starting positions
+// So it sets it immediately
+rotateHandle(handSeconds, (((seconds/60)*360)-90))
+rotateHandle(handMinutes, (((minutes/60)*360)-90))
+rotateHandle(handHours, (((hours/12)*360)-90))
+
+function updateTime() {
+  // Continue multiplying seconds for easyier transition
+  // Function will fire after 1 second and update then increment second
+  rotateHandle(handSeconds, (((seconds/60)*360)-90))
+  seconds++;
+  rotateHandle(handMinutes, (((minutes+(seconds/60))/60)*360)-90)
+  rotateHandle(handHours, ((hours+(hours/12)*360)-90))
+}
+
+setInterval(updateTime, 1000)
